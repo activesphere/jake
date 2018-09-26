106a58f00fd48b68d5132c114d677e67fbe2f221
Aboobacker MK <aboobackervyd@gmail.com>
2018-09-26 15:29:19 +0530

    Handle 'required' constraint in json schema spec

--- lib/jake/object.ex
+++ lib/jake/object.ex
@@ -1,6 +1,24 @@
 defmodule Jake.Object do
+  def gen(%{"properties" => properties, "required" => required}) do
+    required_elements =
+      properties
+      |> Map.take(required)
+
+    properties
+    |> Enum.take_random(:rand.uniform(map_size(properties)) - 1)
+    |> Map.new()
+    |> Map.merge(required_elements)
+    |> Enum.map(fn {name, spec} ->
+      {name, Jake.gen(spec)}
+    end)
+    |> Enum.into(%{})
+    |> StreamData.fixed_map()
+  end
+
   def gen(%{"properties" => properties}) do
-    Enum.map(properties, fn {name, spec} ->
+    properties
+    |> Enum.take_random(:rand.uniform(map_size(properties)) - 1)
+    |> Enum.map(fn {name, spec} ->
       {name, Jake.gen(spec)}
     end)
     |> Enum.into(%{})
