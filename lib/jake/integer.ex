ea84193f907c93ed0c82fbadaf9e221596d44ada
Aboobacker MK <aboobackervyd@gmail.com>
2018-09-29 19:44:57 +0530

    Support for exclusive maximum and minimum

--- lib/jake/integer.ex
+++ lib/jake/integer.ex
@@ -1,10 +1,26 @@
 defmodule Jake.Integer do
   def gen(spec) do
-    min = Map.get(spec, "minimum", -9_007_199_254_740_991)
-    max = Map.get(spec, "maximum", 9_007_199_254_740_991)
+    min = Map.get(spec, "minimum")
+    max = Map.get(spec, "maximum")
+    exclusive_min = Map.get(spec, "exclusiveMinimum")
+    exclusive_max = Map.get(spec, "exclusiveMaximum")
     multipleOf = Map.get(spec, "multipleOf", 1)
 
-    StreamData.integer(trunc(min / multipleOf)..trunc(max / multipleOf))
+    min_bound =
+      case {min, exclusive_min} do
+        {nil, nil} -> -9_007_199_254_740_991
+        {min, nil} -> min
+        {nil, exclusive_min} -> exclusive_min + 1
+      end
+
+    max_bound =
+      case {max, exclusive_max} do
+        {nil, nil} -> 9_007_199_254_740_991
+        {min, nil} -> max
+        {nil, exclusive_max} -> exclusive_max + 1
+      end
+
+    StreamData.integer(trunc(min_bound / multipleOf)..trunc(max_bound / multipleOf))
     |> StreamData.map(&(&1 * multipleOf))
   end
 end
