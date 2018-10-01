ac3e6299c3ae7a4874728408e841aa6b620c0591
Aboobacker MK <aboobackervyd@gmail.com>
2018-10-01 16:58:13 +0530

    Support for additionalItems

--- lib/jake/array.ex
+++ lib/jake/array.ex
@@ -2,26 +2,35 @@ defmodule Jake.Array do
   def gen(spec) do
     items = Map.get(spec, "items", %{})
     uniq = Map.get(spec, "uniqueItems", false)
+    additional_items = Map.get(spec, "additionalItems", %{})
     max_items = Map.get(spec, "maxItems", 100)
     min_items = Map.get(spec, "minItems", 0)
 
-    case {items, uniq} do
-      {x, true} when x == %{} ->
+    case {items, uniq, additional_items} do
+      {items, true, _} when items == %{} ->
         StreamData.string(:ascii)
         |> StreamData.uniq_list_of(max_length: max_items, min_length: min_items)
 
-      {x, false} when x == %{} ->
+      {items, false, _} when items == %{} ->
         StreamData.string(:ascii)
         |> StreamData.list_of(max_length: max_items, min_length: min_items)
 
-      {x, _} when is_list(x) ->
-        x |> Enum.map(&Jake.gen(&1)) |> StreamData.fixed_list()
+      {items, _uniq, false} when is_list(items) ->
+        items |> Enum.map(&Jake.gen(&1)) |> StreamData.fixed_list()
 
-      {x, true} when is_map(x) ->
-        x |> Jake.gen() |> StreamData.uniq_list_of(max_length: max_items, min_length: min_items)
+      {items, _uniq, additional_items} when is_list(items) ->
+        fixed_items = items |> Enum.map(&Jake.gen(&1)) |> StreamData.fixed_list()
 
-      {x, false} when is_map(x) ->
-        x |> Jake.gen() |> StreamData.list_of(max_length: max_items, min_length: min_items)
+        StreamData.fixed_list([fixed_items, StreamData.list_of(Jake.gen(additional_items))])
+        |> StreamData.map(&List.foldl(&1, [], fn x, acc -> acc ++ x end))
+
+      {items, true, _} when is_map(items) ->
+        items
+        |> Jake.gen()
+        |> StreamData.uniq_list_of(max_length: max_items, min_length: min_items)
+
+      {items, false, _} when is_map(items) ->
+        items |> Jake.gen() |> StreamData.list_of(max_length: max_items, min_length: min_items)
     end
   end
 end
