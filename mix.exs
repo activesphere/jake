2c25999190a6f9fab380e9c1e3aa44482e961902
kanishka-linux <kanishka.linux@gmail.com>
2019-01-25 12:49:01 +0530

    add odgn_json_pointer as dependency

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Jake.MixProject do
       {:stream_data, "~> 0.4"},
       {:jason, "~> 1.1"},
       {:randex, "~> 0.4"},
-      {:ex_json_schema, "~> 0.5.4"}
+      {:ex_json_schema, "~> 0.5.4"},
+      {:odgn_json_pointer, "~> 2.4"}
     ]
   end
 end
