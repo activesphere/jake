80ce5d7357bd280c92ab4ce5dd880f26da362c20
kanishka-linux <kanishka.linux@gmail.com>
2019-01-25 17:32:44 +0530

    add default_path and cache to context

--- lib/jake/context.ex
+++ lib/jake/context.ex
@@ -1,3 +1,7 @@
 defmodule Jake.Context do
-  defstruct root: %{}, child: %{}, size: 0
+  defstruct root: %{},
+            child: %{},
+            size: 0,
+            default_path: File.cwd!(),
+            cache: %{}
 end
