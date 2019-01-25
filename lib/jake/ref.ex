8d8ca2ef148c880642ed208b48c35a1320759717
kanishka-linux <kanishka.linux@gmail.com>
2019-01-25 12:54:02 +0530

    use JSONPointer to resolve json pointer

--- lib/jake/ref.ex
+++ lib/jake/ref.ex
@@ -14,7 +14,8 @@ defmodule Jake.Ref do
       if String.starts_with?(uri, "http") do
         process_http_path(uri)
       else
-        process_local_path(uri) |> get_head_list_path(context.root)
+        {:ok, value} = JSONPointer.get(context.root, uri)
+        value
       end
 
     new_child = Map.drop(spec, ["$ref"]) |> Map.merge(ref_map)
@@ -24,31 +25,6 @@ defmodule Jake.Ref do
 
   def expand_ref(%Context{child: _spec} = context) do
     {context, false}
-  end
-
-  def get_head_list_path(path_list, root_schema) do
-    {head, tail} = Enum.split(path_list, -1)
-
-    head_path =
-      if length(head) > 0 do
-        get_in(root_schema, head)
-      else
-        get_in(root_schema, path_list)
-      end
-
-    tail =
-      if is_list(head_path) do
-        Enum.fetch!(tail, 0)
-      else
-        nil
-      end
-
-    if tail != nil and is_numeric(tail) do
-      {index, ""} = Integer.parse(tail)
-      Enum.fetch!(head_path, index)
-    else
-      get_in(root_schema, path_list)
-    end
   end
 
   def process_http_path(url) do
@@ -65,27 +41,8 @@ defmodule Jake.Ref do
     if is_nil(local) do
       jschema
     else
-      process_local_path(local) |> get_head_list_path(jschema)
-    end
-  end
-
-  def process_local_path(path) do
-    str =
-      String.replace(path, "~0", "~")
-      |> String.replace("#/", "", global: false)
-
-    if String.contains?(str, "~1") do
-      strlist = String.split(str, "/")
-      for n <- strlist, do: String.replace(n, "~1", "/")
-    else
-      String.split(str, "/")
-    end
-  end
-
-  def is_numeric(str) do
-    case Integer.parse(str) do
-      {_num, ""} -> true
-      _ -> false
+      {:ok, value} = JSONPointer.get(jschema, local)
+      value
     end
   end
 end
