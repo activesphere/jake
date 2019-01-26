7231c94b05fc18d1b8e889af12ca6b8a90861bec
kanishka-linux <kanishka.linux@gmail.com>
2019-01-26 14:28:00 +0530

    rename default_path to root_dir

--- lib/jake/context.ex
+++ lib/jake/context.ex
@@ -2,6 +2,6 @@ defmodule Jake.Context do
   defstruct root: %{},
             child: %{},
             size: 0,
-            default_path: File.cwd!(),
+            root_dir: "/",
             cache: %{}
 end
